a90ad606b2e22756ce22c85f5ac353281f9e6685
VJ <somasundaramv@ufl.edu>
2022-08-07 02:58:04 -0400

    adding home page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 //import SignUp from "./components/Login";
 import { CreateAccount } from "./components/CreateAccount";
 import Login from "./components/Login";
+import Home from "./components/Home"
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element = {<MainStore />}/>
+      <Route path="/" element = {<Home />}/>
       <Route path="/signup" element={<CreateAccount/>} />
       <Route path="/login" element = {<Login/>}/>
+      <Route path="/store" element = {<MainStore/>} />
     </Routes>
     </BrowserRouter>
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,6 @@
+import ResponsiveAppBar from "./NavBar"
+
+export default function Home() {
+    console.log("Home")
+    return <div><ResponsiveAppBar/><iframe src="https://salesstrike23.wixsite.com/salesstrike" width="1920" height="1080"></iframe></div>
+}
