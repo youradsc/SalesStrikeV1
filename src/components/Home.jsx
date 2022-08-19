f546d89ddec52a74420688143ceeb0df4c1c736c
JSai S <jsai23@JSais-MacBook-Pro.local>
2022-08-19 13:05:50 -0400

    SEO Base

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,11 @@
 import ResponsiveAppBar from "./NavBar"
+import Page from "./Page"
 //issue fixed
 export default function Home() {
     console.log("Home")
-    return <div style={{height:"100%"}}><ResponsiveAppBar/><div style={{position: "relative",
+    return (
+        <Page title="Home">
+    <div style={{height:"100%"}}><ResponsiveAppBar/><div style={{position: "relative",
         paddingBottom: "56.25%", /* 16:9 */
         paddingTop: "25px", height:"100%"}}><iframe style={{position: "absolute",
             top: 0
@@ -11,4 +14,6 @@ export default function Home() {
             width: "100%",
             height: "100%"}}src="https://salesstrike23.wixsite.com/my-site" width="100%" height="100%"></iframe></div>
 </div>
+</Page>
+    )
 }
